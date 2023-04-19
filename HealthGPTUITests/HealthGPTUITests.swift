05fe8a382e6cfa8c239c3fef28ecc70e3974e87d
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-04-19 18:48:33 +0200

    Add UI Tests to successfully run the app

---
+++ HealthGPTUITests/HealthGPTUITests.swift
@@ -0,0 +1,15 @@
+//
+// This source file is part of the Stanford HealthGPT project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University & Project Contributors
+//
+
+import XCTest
+
+
+final class HealthGPTUITests: XCTestCase {
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+}
