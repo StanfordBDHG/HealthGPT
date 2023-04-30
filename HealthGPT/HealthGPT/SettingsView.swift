17e6b890202bf309151ff51f5638044948abec97
Varun Shenoy <varun.nshenoy@gmail.com>
2023-04-30 00:28:55 -0700

    readded code attribution

--- HealthGPT/HealthGPT/SettingsView.swift
+++ HealthGPT/HealthGPT/SettingsView.swift
@@ -2,6 +2,7 @@
 // This source file is part of the Stanford HealthGPT project
 //
 // SPDX-FileCopyrightText: 2023 Stanford University & Project Contributors
+// Created by Varun Shenoy on 4/13/23.
 //
 
 import SwiftUI
