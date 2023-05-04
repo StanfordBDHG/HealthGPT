cbc71708a72a2b1a635b6c3f12512cc86f95455c
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-05-04 10:08:56 -0400

    Create prompt generator

--- HealthGPT/HealthGPT/MessageHandler.swift
+++ HealthGPT/HealthGPT/MessageHandler.swift
@@ -39,7 +39,9 @@ class MessageHandler: ObservableObject {
 
         do {
             let healthData = try await healthDataFetcher.fetchAndProcessHealthData()
-            let mainPrompt = self.buildMainPrompt(with: healthData)
+
+            let generator = PromptGenerator(with: healthData)
+            let mainPrompt = generator.buildMainPrompt()
 
             Task {
                 isQuerying = true
@@ -63,35 +65,5 @@ class MessageHandler: ObservableObject {
 
     func clearMessages() {
         messages = []
-    }
-
-    private func buildMainPrompt(with healthData: [HealthData]) -> String {
-        let today = Date()
-        var mainPrompt = "You are HealthGPT, an enthusiastic, expert caretaker with a deep understanding in personal health. Given the context, provide a short response that could answer the user's question. Do NOT provide statistics. If numbers seem low, provide advice on how they can improve.\n\nSome health metrics over the past two weeks (14 days) to incorporate is given below. If a value is zero, the user has not inputted anything for that day. Today is \(DateFormatter.localizedString(from: today, dateStyle: .full, timeStyle: .none)). Note that you do not have data about the current day.\n\n"
-
-        for day in 0...13 {
-            let dayData = healthData[day]
-            var mainText = ""
-
-            if let steps = dayData.steps {
-                mainText += "\(Int(steps)) steps,"
-            }
-            if let sleepHours = dayData.sleepHours {
-                mainText += " \(Int(sleepHours)) hours of sleep,"
-            }
-            if let activeEnergy = dayData.activeEnergy {
-                mainText += " \(Int(activeEnergy)) calories burned,"
-            }
-            if let exerciseMinutes = dayData.exerciseMinutes {
-                mainText += " \(Int(exerciseMinutes)) minutes of exercise,"
-            }
-            if let bodyWeight = dayData.bodyWeight {
-                mainText += " \(bodyWeight) lbs of body weight,"
-            }
-
-            mainPrompt += "\(dayData.date): \(mainText.dropLast()) \n"
-        }
-
-        return mainPrompt
     }
 }
