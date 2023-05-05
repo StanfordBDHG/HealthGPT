7689ca81bface5ddb2712c48b3cdd0d0dc50a9dd
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-05-04 21:00:45 -0400

    Update fetch and process health data method

--- HealthGPT/HealthGPT/HealthDataFetcher+Process.swift
+++ HealthGPT/HealthGPT/HealthDataFetcher+Process.swift
@@ -14,48 +14,44 @@ extension HealthDataFetcher {
     ///
     /// - Throws: `HealthDataFetcherError.authorizationFailed` if health data authorization fails.
     func fetchAndProcessHealthData() async throws -> [HealthData] {
-        let success = await requestAuthorization()
+        try await requestAuthorization()
 
-        if success {
-            let calendar = Calendar.current
-            let today = Date()
-            var healthData: [HealthData] = []
+        let calendar = Calendar.current
+        let today = Date()
+        var healthData: [HealthData] = []
 
-            // Create an array of HealthData objects for the last 14 days
-            for day in 1...14 {
-                guard let endDate = calendar.date(byAdding: .day, value: -day, to: today) else { continue }
-                healthData.append(
-                    HealthData(
-                        date: DateFormatter.localizedString(from: endDate, dateStyle: .short, timeStyle: .none)
-                    )
+        // Create an array of HealthData objects for the last 14 days
+        for day in 1...14 {
+            guard let endDate = calendar.date(byAdding: .day, value: -day, to: today) else { continue }
+            healthData.append(
+                HealthData(
+                    date: DateFormatter.localizedString(from: endDate, dateStyle: .short, timeStyle: .none)
                 )
-            }
-
-            healthData = healthData.reversed()
-
-            async let stepCounts = fetchLastTwoWeeksStepCount()
-            async let sleepHours = fetchLastTwoWeeksSleep()
-            async let caloriesBurned = fetchLastTwoWeeksActiveEnergy()
-            async let exerciseTime = fetchLastTwoWeeksExerciseTime()
-            async let bodyMass = fetchLastTwoWeeksBodyWeight()
-
-            let fetchedStepCounts = try? await stepCounts
-            let fetchedSleepHours = try? await sleepHours
-            let fetchedCaloriesBurned = try? await caloriesBurned
-            let fetchedExerciseTime = try? await exerciseTime
-            let fetchedBodyMass = try? await bodyMass
-
-            for day in 0...13 {
-                healthData[day].steps = fetchedStepCounts?[day]
-                healthData[day].sleepHours = fetchedSleepHours?[day]
-                healthData[day].activeEnergy = fetchedCaloriesBurned?[day]
-                healthData[day].exerciseMinutes = fetchedExerciseTime?[day]
-                healthData[day].bodyWeight = fetchedBodyMass?[day]
-            }
-
-            return healthData
-        } else {
-            throw HealthDataFetcherError.authorizationFailed
+            )
         }
+
+        healthData = healthData.reversed()
+
+        async let stepCounts = fetchLastTwoWeeksStepCount()
+        async let sleepHours = fetchLastTwoWeeksSleep()
+        async let caloriesBurned = fetchLastTwoWeeksActiveEnergy()
+        async let exerciseTime = fetchLastTwoWeeksExerciseTime()
+        async let bodyMass = fetchLastTwoWeeksBodyWeight()
+
+        let fetchedStepCounts = try? await stepCounts
+        let fetchedSleepHours = try? await sleepHours
+        let fetchedCaloriesBurned = try? await caloriesBurned
+        let fetchedExerciseTime = try? await exerciseTime
+        let fetchedBodyMass = try? await bodyMass
+
+        for day in 0...13 {
+            healthData[day].steps = fetchedStepCounts?[day]
+            healthData[day].sleepHours = fetchedSleepHours?[day]
+            healthData[day].activeEnergy = fetchedCaloriesBurned?[day]
+            healthData[day].exerciseMinutes = fetchedExerciseTime?[day]
+            healthData[day].bodyWeight = fetchedBodyMass?[day]
+        }
+
+        return healthData
     }
 }
