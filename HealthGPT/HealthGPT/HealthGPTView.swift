c2de2db725d963e1eec8342cc3bc7c3a322d3331
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-10-28 13:20:07 -0400

    Add voice-based interaction

--- HealthGPT/HealthGPT/HealthGPTView.swift
+++ HealthGPT/HealthGPT/HealthGPTView.swift
@@ -7,26 +7,59 @@
 //
 
 import SpeziOpenAI
+import SpeziSpeechSynthesizer
 import SwiftUI
 
 
 struct HealthGPTView: View {
     @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+    @AppStorage(StorageKeys.enableTextToSpeech) private var textToSpeech = StorageKeys.Defaults.enableTextToSpeech
     @EnvironmentObject private var openAPIComponent: OpenAIComponent
     @EnvironmentObject private var healthDataInterpreter: HealthDataInterpreter
     @State private var showSettings = false
-    
+    @StateObject private var speechSynthesizer = SpeechSynthesizer()
+
     
     var body: some View {
+        // swiftlint:disable next closure_body_length
         NavigationView {
             VStack {
                 ChatView($healthDataInterpreter.runningPrompt, disableInput: $healthDataInterpreter.querying)
-                    .navigationBarTitle("WELCOME_TITLE")
+                    .navigationTitle("WELCOME_TITLE")
                     .gesture(
                         TapGesture().onEnded {
                             UIApplication.shared.hideKeyboard()
                         }
                     )
+                    .toolbar {
+                        ToolbarItem(placement: .primaryAction) {
+                            Button(
+                                action: {
+                                    showSettings = true
+                                },
+                                label: {
+                                    Image(systemName: "gearshape")
+                                        .accessibilityLabel(Text("OPEN_SETTINGS"))
+                                }
+                            )
+                        }
+                        ToolbarItem(placement: .primaryAction) {
+                            Button(
+                                action: {
+                                    textToSpeech.toggle()
+                                },
+                                label: {
+                                    if textToSpeech {
+                                        Image(systemName: "speaker")
+                                            .accessibilityLabel(Text("SPEAKER_ENABLED"))
+                                    } else {
+                                        Image(systemName: "speaker.slash")
+                                            .accessibilityLabel(Text("SPEAKER_DISABLED"))
+                                    }
+                                }
+                            )
+                        }
+                    }
             }
             .onAppear {
                 generatePrompt()
@@ -34,24 +67,19 @@ struct HealthGPTView: View {
             .onChange(of: completedOnboardingFlow) { _ in
                 generatePrompt()
             }
+            .onChange(of: healthDataInterpreter.querying) { _ in
+                if textToSpeech,
+                    healthDataInterpreter.runningPrompt.last?.role == .assistant,
+                    let lastMessageContent = healthDataInterpreter.runningPrompt.last?.content {
+                    speechSynthesizer.speak(lastMessageContent)
+                }
+            }
             .sheet(isPresented: $showSettings) {
                 SettingsView(chat: $healthDataInterpreter.runningPrompt)
             }
-            .navigationBarItems(
-                trailing:
-                    Button(
-                        action: {
-                            showSettings = true
-                        },
-                        label: {
-                            Image(systemName: "gearshape")
-                        }
-                    )
-            )
         }
     }
-    
-    
+
     private func generatePrompt() {
         _Concurrency.Task {
             guard completedOnboardingFlow else {
